b96d4570f1617c3c5d659703016346fc3b5cba08
Iago Freitas <iagofreitas05@gmail.com>
2022-03-25 20:44:25 -0300

    first

---
+++ src/test/java/CamelCaseTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+public class CamelCaseTest {
+    /* cenários de teste
+    * 1 - O usuário insira uma String Vazia
+    * 2 - O usuário não insira uma String ( um número ou algo do genero)
+    * 3 - Converter o camelCase Recebido  em palavras separadas
+    * */
+
+
+    @Test
+    public void CamelCaseCallEmpty(){
+
+    }
+
+}
